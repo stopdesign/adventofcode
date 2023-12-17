1ec633562bec7b4409361c328e7cb6b4ca977684
Gregory <gregory@stopdesign.ru>
2023-12-16 20:06:17 -0800

    day 13, second part

--- 2023/day_13/code_2.py
+++ 2023/day_13/code_2.py
@@ -17,7 +17,7 @@ data = """
 """
 
 
-def symmetry_line_index(pattern: list[str]) -> int:
+def symmetry_line_index(pattern: list[str], smudge_cnt=0) -> int:
     """
     Returns the index of the row that is the horizontal symmetry line.
     """
@@ -25,10 +25,19 @@ def symmetry_line_index(pattern: list[str]) -> int:
         before = pattern[:i]
         after = pattern[i:]
 
-        min_len = min(len(before), len(after))
-        if after[:min_len] == before[::-1][:min_len]:
+        min_common_size = min(len(before), len(after))
+
+        a_lines = after
+        b_lines = before[::-1]
+
+        # Count difference between A and B patterns
+        diff = 0
+        for j in range(min_common_size):
+            diff += sum(1 for a, b in zip(a_lines[j], b_lines[j]) if a != b)
+
+        if diff == smudge_cnt:
             return i
-            
+
     return 0
 
 
@@ -39,11 +48,12 @@ def transpose(pattern: list[str]) -> list[str]:
 def main(data):
     patterns = data.split("\n\n")
     res = 0
+    smudge_cnt = 1
     for pattern in patterns:
         pattern = pattern.strip().split("\n")
         transposed_pattern = transpose(pattern)
-        sym_h = symmetry_line_index(pattern)
-        sym_v = symmetry_line_index(transposed_pattern)
+        sym_h = symmetry_line_index(pattern, smudge_cnt)
+        sym_v = symmetry_line_index(transposed_pattern, smudge_cnt)
         # print(sym_v, sym_h)
         res += sym_v + sym_h * 100
     print(res)
